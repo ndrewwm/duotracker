bd6100f586bec4ba105a450f341aeb3ff2fe12f4
ndrewwm <moore.andrew09@gmail.com>
2023-04-09 15:35:57 -0600

    Drop the dependency caching

--- R/job.R
+++ R/job.R
@@ -3,6 +3,8 @@ library(readr)
 library(RSelenium)
 library(lubridate)
 
+source("get_profile_details.R")
+
 run <- get_profile_metrics("amateurthoughts")
 
 write_csv(run, "./data-raw/metrics.csv", append = TRUE)
